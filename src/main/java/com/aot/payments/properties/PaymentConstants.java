b5803cf496fc53ee66060612bea57a63996db993
Mohanram <akiladevi.mohanram@accenture.com>
2020-04-27 22:51:18 +0530

    Add AOTCustomerPayment Java files

---
+++ src/main/java/com/aot/payments/properties/PaymentConstants.java
@@ -0,0 +1,6 @@
+package com.aot.payments.properties;
+
+public class PaymentConstants {
+    public final static String paymentInboundPath = "C:\\codes\\extras\\payments\\inbound\\";
+    public final static String paymentOutboundPath = "C:\\codes\\extras\\payments\\outbound\\";
+}
